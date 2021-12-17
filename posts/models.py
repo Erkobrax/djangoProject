ac11dfe3f08b9022925a4ab96c12519f0419fef6
Erkobrax <erkobraxx@gmail.com>
2021-12-17 13:35:44 +0300

    Add class Post and HashTag

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from user_profile.models import User
+
 
 # Create your models here.
+class Post(models.Model):
+    user = models.ForeignKey(User, on_delete=models.PROTECT)
+    text = models.CharField(max_length=300)
+    created_date = models.DateTimeField(auto_now_add=True)
+    is_active = models.BooleanField(default=True)
+    is_favourite = models.BooleanField(default=False)
+
+    def __init__(self):
+        return self.text
+
+
+class HashTag(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    post = models.ManyToManyField(Post)
+
+    def __init__(self):
+        return self.name
