b17ea6790bf80e059d3c96bbc2a7c3790f1874d8
Erkobrax <erkobraxx@gmail.com>
2021-12-17 13:37:45 +0300

    add class Profile and Index

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+from django.views import View
+from .models import Post
+from user_profile.models import User
+
+
+class Index(View):
+
+    def get(self, request):
+        context = {'text': 'Hello world'}
+        return render(request, 'base.html', context)
+
+
+class Profile(View):
+    def get(self, request, username):
+        user = User.objects.get(username=username)
+        posts = Post.objects.filter(user=user)
+        context = {
+            'posts': posts,
+            'user': user,
+        }
+        return render(request, 'profile.html', context)
