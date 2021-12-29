07ffa2d61877cbcf99b17d2b893f42ead50882b9
Erkobrax <erkobraxx@gmail.com>
2021-12-30 00:52:01 +0300

    add functional of contactform, registeruser, logout, login

--- posts/views.py
+++ posts/views.py
@@ -1,20 +1,54 @@
-from django.shortcuts import render
-from django.http import HttpResponseRedirect
+from django.contrib.auth import login, logout
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.views import LoginView
+from django.shortcuts import render, redirect
+from django.http import HttpResponseRedirect, HttpResponseNotFound
 from django.http import HttpResponse
 # Create your views here.
+from django.urls import reverse_lazy
 from django.views import View
+from django.views.generic import CreateView, FormView
+
 from .models import Post, HashTag
 from user_profile.models import User
-from posts.forms import PostForm, SearchForm
+from user_profile.models import User
+from posts.forms import PostForm, SearchForm, SearchTagForm, LoginUserForm, RegisterUserForm, ContactForm
 from django.template.loader import render_to_string
 import json
+
+from .utils import DataMixin
+
+menu = [{'title':"About website",'url_name':'about'},
+        {'title':"Add post", 'url_name':'profile '},
+        {'title':"FeedBack", 'url_name':'contact'}
+]
 
 
 class Index(View):
 
     def get(self, request):
-        context = {'text': 'Hello world'}
+        context = {'menu': menu, 'text': 'Hello world'}
         return render(request, 'base.html', context)
+
+
+def about(request):
+    contact_list = User.objects.all()
+    return render(request, 'about.html', )
+
+
+class ContactFormView(DataMixin, FormView):
+    form_class = ContactForm
+    template_name = 'contact.html'
+    success_url = reverse_lazy('home')
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        c_def = self.get_user_context(title="Feedback")
+        return dict(list(context.items()) + list(c_def.items()))
+
+    def form_valid(self, form):
+        print(form.cleaned_data)
+        return redirect('home')
 
 
 class Profile(View):
@@ -76,3 +110,52 @@ class SearchTag(View):
         tags = HashTag.objects.filter(name__icontains=q)
         context = {'tags': tags, 'searchtag': form}
         return render(request, 'search_tags.html', context)
+
+
+class TagJson(View):
+    def get(self, request):
+        q = request.GET.get('q', '')
+        taglist = []
+        tags = HashTag.objects.filter(name__icontains=q)
+        for tag in tags:
+            new = {'q': tag.name, 'count': int(len(tag.post.all()))}
+            taglist.append(new)
+        return HttpResponse(json.dumps(taglist), content_type="application/json")
+
+
+class LoginUser(DataMixin, LoginView):
+    form_class = LoginUserForm
+    template_name = 'registration/login.html'
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        c_def = self.get_user_context(title="Authentication")
+        return dict(list(context.items()) + list(c_def.items()))
+
+    def get_success_url(self):
+        return reverse_lazy('home')
+
+
+class RegisterUser(DataMixin, CreateView):
+    form_class = RegisterUserForm
+    template_name = 'registration/register.html'
+    success_url = reverse_lazy('login')
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        c_def = self.get_user_context(title="Registration")
+        return dict(list(context.items()) + list(c_def.items()))
+
+    def form_valid(self, form):
+        user = form.save()
+        login(self.request, user)
+        return redirect('home')
+
+
+def pageNotFound(request, exception):
+    return HttpResponseNotFound('<h1>Page not found</h1>')
+
+
+def logout_user(request):
+    logout(request)
+    return redirect('login')
