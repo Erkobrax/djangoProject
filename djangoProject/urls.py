fd1e75e758fb373aaa286680aff4f1c6770d59cc
Erkobrax <erkobraxx@gmail.com>
2021-12-30 00:51:00 +0300

    add path for user,search,hashtag,register,login,logout

--- djangoProject/urls.py
+++ djangoProject/urls.py
@@ -13,14 +13,31 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, re_path
+from django.contrib.auth import views as auth_views
+
+from djangoProject import settings
 from posts import views
-from posts.views import Index, Profile, PostPost, Search
+from posts.views import *
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', Index.as_view(), name='home'),
-    re_path(r'^user/(\w+)/$', Profile.as_view()),
-    re_path(r'^user/(\w+)/post/$', PostPost.as_view()),
+    re_path(r'^user/(\w+)/$', Profile.as_view(), name='profile'),
+    re_path(r'^user/(\w+)/post/$', PostPost.as_view(), name='post'),
     re_path(r'^search/$', Search.as_view()),
+    re_path(r'^search/hashtag$', SearchTag.as_view()),
+    re_path(r'^hashtag.json', TagJson.as_view()),
+    re_path(r'^login/$', LoginUser.as_view(), name='login'),
+    re_path(r'^register/$', RegisterUser.as_view(), name='register'),
+    path('contact/', ContactFormView.as_view(), name='contact'),
+    path('about/', about, name='about'),
+    path('logout/', logout_user, name='logout')
+
+
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+handler404 = pageNotFound
